0664d18cc9c1dbdeeb8f75e89a8447173e7e0fa3
Samaan Kamal <samaankamal72@gmail.com>
2024-05-16 21:50:06 +0300

    added user list component using the cli and register it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule
