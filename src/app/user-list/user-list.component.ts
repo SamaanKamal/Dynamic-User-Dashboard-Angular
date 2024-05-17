e9b2a4f346539e75d800d1dbc14c3ce6c7d589fa
Samaan Kamal <samaankamal72@gmail.com>
2024-05-17 18:32:49 +0300

    added list structure in users list component

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../shared/User.Model';
+import { UserService } from '../shared/User-Service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  users: User[] = [];
+  error: string = null;
+  isLoading = false;
+  currentPage = 1;
 
+  constructor(private userService: UserService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.userService.fetchUsers(this.currentPage).subscribe({
+      next: (responseData) => {
+        this.users = responseData.data;
+        console.log(responseData.data);
+      },
+      error: (errorMessage) => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+      },
+    });
+  }
+  onPageChange(page: number): void {
+    this.currentPage = page;
+    this.getUsers();
+  }
+
+  viewDetails(id: number) {
+    this.router.navigate(['/users',id])
+  }
 }
