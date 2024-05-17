01dee4fd18b1af06ea411181103ba4c748ed4555
Samaan Kamal <samaankamal72@gmail.com>
2024-05-17 22:46:17 +0300

    added search input and search functionality to the header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  searchQuery: string = '';
+  error: string = null;
+  constructor(private router: Router) {}
 
+
+  search() {
+    const userId = parseInt(this.searchQuery);
+    if (!isNaN(userId)) {
+      this.router.navigate(['/user', userId]);
+      this.error = null;
+    }
+    else {
+      this.error = "please enter a valid user ID";
+    }
+  }
 }
